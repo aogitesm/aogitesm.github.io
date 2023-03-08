7d34072da441ff2e68651f6c2de96d15210c8b90
Arturo <a00829992@tec.mx>
2023-03-08 09:44:46 -0600

    Initial commit

---
+++ src/app/cv-body/cv-body.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cv-body',
+  templateUrl: './cv-body.component.html',
+  styleUrls: ['./cv-body.component.css']
+})
+export class CvBodyComponent {
+
+}
